f9f97131dc362dccd474d7d8bb7ab2fdc4bba9e6
Anirudh Mittal <anirudh7795@gmail.com>
2019-04-13 16:47:42 -0700

    Added tooltip and server route for user details

--- public/index.js
+++ public/index.js
@@ -2,40 +2,71 @@ function ShowOrHideLoadIcon(param) {
 	document.getElementsByClassName("lds-dual-ring")[0].style.display = param;
 }
 
+function RemoveToolTip(tooltipId) {
+	let tooltip = document.getElementById(`tooltip${tooltipId}`);
+	tooltip.style.visibility = 'hidden';
+	tooltip.style.display = 'none';
+}
+
+function FetchUserDetails(userId, tooltipId) {
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == XMLHttpRequest.DONE) {
+			if(xmlhttp.status === 200){
+				const userData = JSON.parse(xmlhttp.responseText);
+				console.log(userData.username);
+				let tooltip = document.getElementById(`tooltip${tooltipId}`);
+				tooltip.innerHTML = userData.username;
+				tooltip.style.visibility = 'visible'; 
+				tooltip.style.display = 'block';
+
+			} else {
+				alert(`error ${xmlhttp.status}`);
+			}
+		}
+	}
+	xmlhttp.open('GET',`user?userId=${userId}`,true);
+	xmlhttp.send();
+}
+
+
 function Populate(data) {
 	console.log(data);
 	let cards = document.getElementsByClassName("card-container");
-	data.projects.forEach(project => {
-		let date = new Date(project.updated*1000);
-		let hours = date.getHours();
-		let minutes = '0' + date.getMinutes();
-		let seconds = '0' + date.getSeconds();
-		let lastUpdatedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+	data.projects.forEach((project,idx) => {
+		const date = new Date(project.updated*1000);
+		const months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+ 		const year = date.getFullYear();
+ 		const month = months_arr[date.getMonth()];
+ 		const day = date.getDate();
+		let lastUpdatedTime = `${day} ${month} ${year}`;
 
 		let article = document.createElement('article');
 		article.className="card";
-		// article.style.backgroundImage = `url(${project.image_url})`;
-
 		article.innerHTML = `<header class="card__title">
             					<h3>${project.name}</h3>
         					</header>
         					<main class="card__description">
             					<p>${project.summary}</p>
-            					<p>User ID: ${project.owner_id}</p>
+            					<div onmouseover="FetchUserDetails(${project.owner_id}, ${idx})" onmouseout="RemoveToolTip(${idx})">User ID: ${project.owner_id}
+            						<div id="tooltip${idx}" class="tooltiptext"> </div>
+            					</div>
             					<ul class="list">
             						<li>Followers: ${project.followers} </li>
             						<li>Skulls: ${project.skulls} </li>
             						<li>Comments: ${project.comments} </li>
             					</ul>
         					</main>
-        					<a href="${project.url}" class="button" target="_blank">See Project</a>`;
+        					<a href="${project.url}" class="button" target="_blank">See Project</a>
+        					<footer>
+  								<p><small>Last updated: ${lastUpdatedTime}</small></p>
+  							</footer>`;
         cards[0].appendChild(article);
 	})
 }
 
 
 function GetProjects(pageNum=1) {
-
 	ShowOrHideLoadIcon("block");
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
@@ -52,5 +83,4 @@ function GetProjects(pageNum=1) {
 	}
 	xmlhttp.open('GET',`projects?page=${pageNum}`,true);
 	xmlhttp.send();
-
 }
